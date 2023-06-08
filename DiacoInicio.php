7fe85112c9ab1cbb43f5c284222fc87a411be8b0
Pablo Olivares <polivaress@miumg.edu.gt>
2023-06-08 00:00:30 -0600

    terminando de configurar el carrusel

--- DiacoInicio.php
+++ DiacoInicio.php
@@ -127,19 +127,19 @@
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active" data-bs-interval="10000">
-                        <img src="" class="d-block w-100" alt="...">
+                        <img src="assets/images/DIACO-1000x600.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
 
                         </div>
                     </div>
                     <div class="carousel-item" data-bs-interval="2000">
-                        <img src="" class="d-block w-100" alt="...">
+                        <img src="assets/images/Verificaciones-Diaco-2.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
 
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="" class="d-block w-100" alt="...">
+                        <img src="assets/images/verificaciones-diaco-scaled.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
 
                         </div>
